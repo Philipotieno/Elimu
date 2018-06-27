af0276fde76f858401e079a2c05b4f03278f019d
Philip Otieno <tmitchellb007@gmail.com>
2018-06-27 17:42:04 +0300

    Log in and conditionals

--- Simple command/register.py
+++ Simple command/register.py
@@ -1,7 +1,21 @@
-def reg():
-            name = input("Full name: ")
-            email = input("Enter your email: ")
-            username = input("Prefered username: ")
-            password = input("Password: ")
-            print("welcome ", name, "your username and is ", username)
-reg()
+
+log_reg = eval(input("To register press 1 and enter, to log in press 2 and enter: "))
+
+if log_reg == 1:
+    def reg():
+        name = input("Full name: ")
+        email = input("Enter your email: ")
+        username = input("Prefered username: ")
+        password = input("Password: ")
+        print("welcome ", name, "your username and is ", username)
+
+    reg()
+    
+elif log_reg == 2:
+    def login ():
+
+        username = input("username: ")
+        password = input("Password: ")
+        print("You are now logged in as ", username)
+
+    login()
