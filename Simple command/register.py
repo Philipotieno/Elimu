329f1c5b74ff05020e4c2f3fd66b0a7ce6e5c95f
Philip Otieno <tmitchellb007@gmail.com>
2018-06-27 17:32:31 +0300

    registration

---
+++ Simple command/register.py
@@ -0,0 +1,7 @@
+def reg():
+            name = input("Full name: ")
+            email = input("Enter your email: ")
+            username = input("Prefered username: ")
+            password = input("Password: ")
+            print("welcome ", name, "your username and is ", username)
+reg()
