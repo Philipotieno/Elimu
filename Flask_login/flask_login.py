154ebe59f1e8dba6444830210158552f7bf5f030
Philip Otieno <tmitchellb007@gmail.com>
2018-06-30 18:08:35 +0300

    Post and log out

--- Flask_login/flask_login.py
+++ Flask_login/flask_login.py
@@ -3,45 +3,47 @@ from flask import Flask, session, render_template, request,redirect, g, url_for
 app = Flask(__name__)
 app.secret_key = "philip"
 
+@app.before_request
+def before_request():
+	g.user = None
+	if 'user' in session:
+		g.user = session['user']
 
-#main route
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	if request.method == "POST":
-		session.pop('username',None)
+	if request.method == 'POST':
+		session.pop('user', None)
 
 		if request.form['password'] == 'password':
-			session['username'] = request.form['username']
+			session['user'] = request.form['username']
 			return redirect(url_for('post'))
 
 	return render_template('index.html')
+
 
 @app.route('/post')
 def post():
 	if g.user:
 		return render_template('post.html')
-	else:
-		return redirect(url_for('index'))
+
+	return redirect(url_for('index'))
 
 
-@app.before_request
-def before_request():
-	g.user = None
-	if 'user' in session:
-		g.user = session['username']
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 def login():
-	if request.method == "POST":
-		return session['username']
+	if 'user' in session:
+		return redirect(url_for('index'))
 	else:
-		return render_template('login.html')
+		return 'Not logged in!'
 
 @app.route('/logout')
 def logout():
-	session.pop('username', None)
-	return '<h2>You are logged out</h2>'
+	session.pop('user')
+
+	return "<h2>Logged out</h2>"
+
 
 
 if __name__ == '__main__':
-	app.run(debug=True, port=5045)
+	app.run(debug=True, port=5070)
