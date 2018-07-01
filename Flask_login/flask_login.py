bd13e61dd8bd5fe84952c418c412dffa13015f2a
Philip Otieno <tmitchellb007@gmail.com>
2018-07-01 14:58:25 +0300

    unit tests and details

--- Flask_login/flask_login.py
+++ Flask_login/flask_login.py
@@ -1,49 +1,71 @@
-from flask import Flask, session, render_template, request,redirect, g, url_for
+ #!/usr/bin/env python
+'''
+Flask app where logged in users can:
+comment up
+fetch details
+fetch comments
+view user details
+'''
+from flask import Flask, session, render_template, request, redirect, g, url_for
 
-app = Flask(__name__)
+app = Flask(__name__) # pylint: disable=invalid-name
 app.secret_key = "philip"
 
-@app.before_request
+@app.before_request  # pylint: disable=invalid-name
 def before_request():
-	g.user = None
-	if 'user' in session:
-		g.user = session['user']
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])  # pylint: disable=invalid-name
 def index():
-	if request.method == 'POST':
-		session.pop('user', None)
+    if request.method == 'POST':
+        session.pop('user', None)
 
-		if request.form['password'] == 'password':
-			session['user'] = request.form['username']
-			return redirect(url_for('post'))
+        if request.form['password'] == 'password':
+            session['user'] = request.form['username']
+            return redirect(url_for('post'))
 
-	return render_template('index.html')
+    return render_template('index.html')
 
 
-@app.route('/post')
+@app.route('/post') # pylint: disable=invalid-name
 def post():
-	if g.user:
-		return render_template('post.html')
+    if g.user:
+        return render_template('post.html')
 
-	return redirect(url_for('index'))
+    return redirect(url_for('index'))
+
+@app.route('/comment') # pylint: disable=invalid-name
+def comment():
+    if g.user:
+        return render_template('comment.html')
+
+    return redirect(url_for('index'))
+
+@app.route('/details', methods=['GET', 'POST']) # pylint: disable=invalid-name
+def details():
+    if 'user' in session:
+    	text = request.form['name']
+    	processed_text = text.upper()
+    	return processed_text
+
+    return render_template('index.html')
 
 
-
-@app.route('/login')
+@app.route('/login') # pylint: disable=invalid-name
 def login():
-	if 'user' in session:
-		return redirect(url_for('index'))
-	else:
-		return 'Not logged in!'
+    if 'user' in session:
+        return redirect(url_for('post'))
 
-@app.route('/logout')
-def logout():
-	session.pop('user')
+    return render_template('index.html')
 
-	return "<h2>Logged out</h2>"
+@app.route('/logout') # pylint: disable=invalid-name
+def logout(): #user can log out
+    session.pop('user')
+    return "<h2>Logged out</h2>"
 
 
 
 if __name__ == '__main__':
-	app.run(debug=True, port=5070)
+    app.run(debug=True, port=5054)
---
+++ Flask_login/test_flask_login.py
@@ -0,0 +1,12 @@
+import unittest
+from pylint.testutils import make_tests, LintTestUsingFile, cb_test_gen, linter
+import flask_login
+from flask_login import *
+
+class LoginSessions(unittest.TestCase):
+
+    def setUp(self):
+    	pass
+
+if __name__ == '__main__':
+    unittest.main()
