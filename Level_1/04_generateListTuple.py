671c5accf0fe693218fe33a7da2a1d61d7777652
philipotieno <tmitchellb007@gmail.com>
2020-02-20 00:16:18 +0300

    generates a list and tuple from a comma separated values

---
+++ Level_1/04_generateListTuple.py
@@ -0,0 +1,11 @@
+# Write a program which accepts a sequence of comma-separated numbers from console
+# and generate a list and a tuple which contains every number.
+
+values = input()
+
+l = values.split(",")
+
+t = tuple(l)
+
+print("List = ", l)
+print("Tuple = ", t)
